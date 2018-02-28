252d608f55a6c3c3c0f4c8d18bbcab9278afb802
Alexander Weidinger <alexander.weidinger@tum.de>
2018-02-28 18:25:38 +0100

    Fix generation of pb.cc, pb.h and .o files into build directory.

--- lib/import/import-libprotobuf.mk
+++ lib/import/import-libprotobuf.mk
@@ -3,11 +3,9 @@ INC_DIR += $(LIBPROTOBUF_PORT_DIR)/src/lib/libprotobuf/src
 
 PROTOBUF_PROTOC := $(BUILD_BASE_DIR)/tool/protobuf/protoc
 
-$(TARGET): $(subst proto,pb.cc,$(SRC_PROTO))
+# replace *.proto by *.pb.cc
+SRC_CC += $(subst proto,pb.cc,$(SRC_PROTO))
 
+# compile *.proto to *.pb.cc and *.pb.h
 %.pb.cc: %.proto
-	$(PROTOBUF_PROTOC) --cpp_out=$(PRG_DIR) -I $(PRG_DIR) $<
-	$(eval SRC_CC += $@)
-	@echo $(SRC_CC)
-
-vpath $(subst proto,pb.cc,$(SRC_PROTO)) $(PRG_DIR)
+	$(VERBOSE)$(PROTOBUF_PROTOC) --cpp_out=. -I $(PRG_DIR) $<
