53b77ba65b6c1ec3242baee1dfbce12233ec21da
Alexander Weidinger <alexander.weidinger@tum.de>
2018-03-08 16:26:10 +0100

    Make libprotobuf compile proto files given abs and rel path.

--- lib/import/import-libprotobuf.mk
+++ lib/import/import-libprotobuf.mk
@@ -3,9 +3,16 @@ INC_DIR += $(LIBPROTOBUF_PORT_DIR)/src/lib/libprotobuf/src
 
 PROTOBUF_PROTOC := $(BUILD_BASE_DIR)/tool/protobuf/protoc
 
-# replace *.proto by *.pb.cc
-SRC_CC += $(subst proto,pb.cc,$(SRC_PROTO))
+# replace *.proto by *.pb.cc and add files to SRC_CC
+SRC_CC += $(notdir $(SRC_PROTO:.proto=.pb.cc))
 
 # compile *.proto to *.pb.cc and *.pb.h
-%.pb.cc: %.proto
-	$(VERBOSE)$(PROTOBUF_PROTOC) --cpp_out=. -I $(PRG_DIR) $<
+%.pb.cc:
+	@echo "    GENERATE "$@
+	$(eval PROTO_FILE := $(filter $(addprefix %,$(@:.pb.cc=.proto)),$(SRC_PROTO)))
+	$(VERBOSE) if [[ $(PROTO_FILE) != /* ]]; \
+	then    # relative path given \
+		$(PROTOBUF_PROTOC) --cpp_out=./ $(addprefix -I$(PRG_DIR)/,$(dir $(SRC_PROTO))) $(PRG_DIR)/$(PROTO_FILE); \
+	else    # absolute path given \
+		$(PROTOBUF_PROTOC) --cpp_out=./ $(addprefix -I,$(dir $(SRC_PROTO))) $(PROTO_FILE); \
+	fi;
